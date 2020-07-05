29dc7748a61fd1fc8e81bd65cda6240475aa4902
Jiazheng Zhu <zhujiazheng98@hotmail.com>
2020-07-05 02:23:22 +0100

    added intercept estimates

--- mr_sim.R
+++ mr_sim.R
@@ -64,13 +64,6 @@ simulate = function(nsamples, alpma_max, beta, xerror, measerror, yerror){
   return(list(mr_mvegger(mrmv), mr_mvivw(mrmv)))
 }
 
-egg_CI_lower = matrix(0, nrow = trials, ncol = 3)
-egg_CI_upper = matrix(0, nrow = trials, ncol = 3)
-egg_est = matrix(0, nrow = trials, ncol = 3)
-ivw_CI_lower = matrix(0, nrow = trials, ncol = 3)
-ivw_CI_upper = matrix(0, nrow = trials, ncol = 3)
-ivw_est = matrix(0, nrow = trials, ncol = 3)
-results = data.frame()
 #true values
 beta = c(0.2,-0.6,0.0)
 alpha_list = list(c(1,1,1),
@@ -96,18 +89,32 @@ yerror = 0.1
 trials = 2000
 nsamples = 1000
 
+egg_CI_lower = matrix(0, nrow = trials, ncol = 3)
+egg_CI_upper = matrix(0, nrow = trials, ncol = 3)
+egg_est = matrix(0, nrow = trials, ncol = 3)
+egg_int_CI_lower = matrix(0, nrow = trials, ncol = 1)
+egg_int_CI_upper = matrix(0, nrow = trials, ncol = 1)
+egg_int_est = matrix(0, nrow = trials, ncol = 1)
+ivw_CI_lower = matrix(0, nrow = trials, ncol = 3)
+ivw_CI_upper = matrix(0, nrow = trials, ncol = 3)
+ivw_est = matrix(0, nrow = trials, ncol = 3)
+results = data.frame()
+
 pb = progress_bar$new(format = " [:bar] :percent eta: :eta",total = trials*length(measerror_list)*length(alpha_list))
 for (alphamax in alpha_list){
   for (measerror in measerror_list){
     egg_coverage_count = ivw_coverage_count = rep(trials,3)
     egg_pwr_count = ivw_pwr_count = rep(trials,3)
-    
+    egg_int_coverage_count = 0
     for (i in 1:trials){
       pb$tick()
       m = simulate(nsamples, alphamax, beta, xerror, measerror, yerror)
       egg_CI_lower[i,] = m[[1]]@CILower.Est
       egg_CI_upper[i,] = m[[1]]@CIUpper.Est
       egg_est[i,] = m[[1]]@Estimate
+      egg_int_CI_lower[i,] = m[[1]]@CILower.Int
+      egg_int_CI_upper[i,] = m[[1]]@CIUpper.Int
+      egg_int_est[i,] = m[[1]]@Intercept
       ivw_CI_lower[i,] = m[[2]]@CILower
       ivw_CI_upper[i,] = m[[2]]@CIUpper
       ivw_est[i,] = m[[2]]@Estimate
@@ -119,6 +126,7 @@ for (alphamax in alpha_list){
       egg_pwr_count[j] = sum(egg_CI_lower[,j] > 0) + sum(egg_CI_upper[,j] < 0)
       ivw_pwr_count[j] = sum(ivw_CI_lower[,j] > 0) + sum(ivw_CI_upper[,j] < 0)
     }
+    egg_int_coverage_count = sum(egg_int_CI_lower[,1] > 0) + sum(egg_int_CI_upper[,1] < 0)
     
     results = rbind(results, data.frame(toString(alphamax),toString(measerror),
                                         mean(egg_est[,1]),sd(egg_est[,1]),egg_coverage_count[1]/trials,egg_pwr_count[1]/trials,
@@ -126,7 +134,8 @@ for (alphamax in alpha_list){
                                         mean(egg_est[,2]),sd(egg_est[,2]),egg_coverage_count[2]/trials,egg_pwr_count[2]/trials,
                                         mean(ivw_est[,2]),sd(ivw_est[,2]),ivw_coverage_count[2]/trials,ivw_pwr_count[2]/trials,
                                         mean(egg_est[,3]),sd(egg_est[,3]),egg_coverage_count[3]/trials,egg_pwr_count[3]/trials,
-                                        mean(ivw_est[,3]),sd(ivw_est[,3]),ivw_coverage_count[3]/trials,ivw_pwr_count[3]/trials))
+                                        mean(ivw_est[,3]),sd(ivw_est[,3]),ivw_coverage_count[3]/trials,ivw_pwr_count[3]/trials,
+                                        mean(egg_int_est),egg_int_coverage_count/trials))
     
   }
 }
